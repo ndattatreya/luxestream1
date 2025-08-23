4e5f20f439f4334b19a8cfe78b7927067fe1627d
Nammina Dattatreya <dattatreya.nammina1@gmail.com>
2025-08-23 14:05:51 +0530

    updated signup

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -15,7 +15,7 @@ const SignUp = () => {
     setSuccess('');
 
     try {
-      const response = await fetch('http://localhost:10000/api/signup', {
+      const response = await fetch('http://localhost:5000/api/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
