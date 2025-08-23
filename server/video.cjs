37d7b7e564fd765e0daa76d8e4419ed8a8b458d3
Nammina Dattatreya <147964790+ndattatreya@users.noreply.github.com>
2025-08-23 13:32:57 +0530

    Update video.cjs

--- server/video.cjs
+++ server/video.cjs
@@ -4,7 +4,7 @@ const path = require('path');
 const fetch = require('node-fetch'); // install this: npm install node-fetch@2
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON
 app.use(express.json());
