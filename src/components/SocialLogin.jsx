1aae2082fbfcc2a1aac2a09cd7d20004cf5dc53e
Nammina Dattatreya <147964790+ndattatreya@users.noreply.github.com>
2025-11-22 09:17:51 +0530

    Update SocialLogin.jsx

--- src/components/SocialLogin.jsx
+++ src/components/SocialLogin.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
-const SocialLogin = () => (
-  <div className="flex flex-col gap-2 my-4">
-    <a
-      href="http://localhost:5000/auth/google"
-      className="bg-white text-black border border-gray-300 py-2 rounded flex items-center justify-center hover:bg-gray-100"
-    >
-      <img src="/google-icon.svg" alt="Google" className="w-5 h-5 mr-2" />
-      Continue with Google
-    </a>
-  </div>
-);
+const SocialLogin = () => {
+  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
-export default SocialLogin; 
+  return (
+    <div className="flex flex-col gap-2 my-4">
+      <a
+        href={`${BACKEND_URL}/auth/google`}
+        className="bg-white text-black border border-gray-300 py-2 rounded flex items-center justify-center hover:bg-gray-100"
+      >
+        <img src="/google-icon.svg" alt="Google" className="w-5 h-5 mr-2" />
+        Continue with Google
+      </a>
+    </div>
+  );
+};
+
+export default SocialLogin;
